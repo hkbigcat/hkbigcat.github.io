d12144629b37bfa8917e834dca60f2057b0b98c4
Ben Lee <benleehk@gmail.com>
2022-11-16 14:28:03 +0800

    set nav zindex

--- scripts.js
+++ scripts.js
@@ -8,17 +8,23 @@ document.addEventListener("click" , e=> {
     if (!isHamburger && isNav!= null && isCloseBtn == null && isNavLink==null)  return; 
     if (isHamburger) {
 
-        if (w>1279)  document.getElementById("mySidenav").style.width = "320px"; else {
+        if (w>1279)  { 
+            document.getElementById("mySidenav").style.width = "320px";
+            
+        } else {
         document.getElementById("mySidenav").style.width = "100%";
         
         }
+        document.getElementById("mySidenav").style.zIndex = "4";
         document.getElementById("mySidenav").style.display = "block";
         //document.getElementById("mySidenav").style.zIndex = 5;
         document.getElementById("mySidenav").style.opacity = "1";
         document.getElementById("hamburger").style.opacity = "0";
     } else {
-        if (w>1279) document.getElementById("mySidenav").style.width = "0"; else document.getElementById("mySidenav").style.display = "none";
-        //document.getElementById("mySidenav").style.zIndex = 1;
+        if (w>1279)  {
+            document.getElementById("mySidenav").style.width = "0";
+        } else document.getElementById("mySidenav").style.display = "none";
+        document.getElementById("mySidenav").style.zIndex = 1;
         document.getElementById("mySidenav").style.opacity = "0";
         document.getElementById("hamburger").style.opacity = "1";
 
