7b2ef20d062154a1caeef4f247e4e7bc9bd25d1d
Ben Lee <ben@stargreemedia.com>
2022-12-04 01:40:20 +0800

    for mr yu

--- js/sample.js
+++ js/sample.js
@@ -11,3 +11,12 @@ sampleclose= () => {
     document.getElementById("sample").style.zIndex=1;
     document.querySelector(".content").style.display="block";
 }
+adjustSample = () => {
+    const p4 = document.getElementById("stockphoto4");
+    const contentHeight = p4.offsetHeight + 80;
+    document.querySelector("#sample").style.paddingBottom=contentHeight+"px"
+}
+window.addEventListener('resize', function(event){
+    adjustSample();
+});     
+adjustSample();
